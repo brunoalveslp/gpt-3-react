9efb6ecb760836f420de2f9bb757d9fba6602e04
Bruno Alves <brunoalveslp200597@gmail.com>
2021-12-30 19:34:06 -0300

    Initial Commit: app structured,navbar and header

---
+++ src/containers/whatGP3/WhatGP3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatGP3.css';
+
+const WhatGP3 = () => {
+    return (
+        <div>
+            WhatGP3
+        </div>
+    );
+}
+
+export default WhatGP3;
