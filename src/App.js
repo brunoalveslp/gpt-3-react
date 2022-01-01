d0533a68abd7a89e62922f15f5d7841fdd0473d6
Bruno Alves <brunoalveslp200597@gmail.com>
2022-01-01 05:01:17 -0300

    final changes ready for production

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { CTA, Brand, Navbar } from './components';
-import { Header, Footer, Features, Blog, Possibility, WhatGP3 } from './containers';
-import './App.css';
+import {CTA, Brand, Navbar} from "./components";
+import {
+	Header,
+	Footer,
+	Features,
+	Blog,
+	Possibility,
+	WhatGP3,
+} from "./containers";
+import "./App.css";
 
-const App = ()=>{
-    return (
-        <div className='App'>
-            <div className='gradient__bg'>
-                <Navbar />
-                <Header />
-            </div>
-            <Brand />
-            <WhatGP3 />
-            <Features />
-            <Possibility />
-            <CTA />
-            <Blog />
-            <Footer />
-        </div>
-    );
+const App = () => {
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGP3 />
+			<Features />
+			<Possibility />
+			<CTA />
+			<Blog />
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
